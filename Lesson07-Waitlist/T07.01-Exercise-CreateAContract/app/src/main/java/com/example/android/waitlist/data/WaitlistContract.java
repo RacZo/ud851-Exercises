af2f0f0a496c3da16dcde1dd1f5e2c48c9f5e1e3
Oscar S <oscar@realactionsoft.com>
2017-06-08 21:46:19 -0400

    fix: Made Unit Test Work
    Implemented BaseColums in Wait List Contract inner class

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -1,12 +1,14 @@
 package com.example.android.waitlist.data;
 
+import android.provider.BaseColumns;
+
 public class WaitlistContract {
 
     private WaitlistContract() {
 
     }
 
-    public class WaitListEntry {
+    public static final class WaitListEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "waitlist";
         public static final String COLUMN_GUEST_NAME = "guestName";
